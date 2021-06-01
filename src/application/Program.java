ed73e93eb7c61a7f4b7f57ec86ed0bd402cd2f38
kaiorobles7 <kaiorobles7@gmail.com>
2021-06-01 15:26:03 -0300

    Checkmate logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,7 +19,7 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		List <ChessPiece> captured = new ArrayList <>();
 		
-		while(true) {
+		while(!chessMatch.getCheckMate()) {
 			try {
 				UI.printMatch(chessMatch, captured);
 				System.out.println();
@@ -48,5 +48,7 @@ public class Program {
 				sc.nextLine();
 			}
 		}
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
 }
